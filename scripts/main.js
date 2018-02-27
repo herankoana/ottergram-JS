ad58ba58e0e2c790f2377f5953418556207debd5
Anthony Hernandez <herankoana@csu.fullerton.edu>
2018-02-26 23:07:49 -0800

    Final Commit
    Next and Previous functionality added. Odd issue where all of my lint changes regarding quotation marks got lost has been fixed.

--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,113 @@
-var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
-var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
-var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var DETAIL_IMAGE_SELECTOR = "[data-image-role=\"target\"]";
+var DETAIL_TITLE_SELECTOR = "[data-image-role=\"title\"]";
+var THUMBNAIL_LINK_SELECTOR = "[data-image-role=\"trigger\"]";
+<<<<<<< HEAD
+var NEXT_BUTTON_SELECTOR = "[data-button-role=\"gonext\"]";
+var PREV_BUTTON_SELECTOR = "[data-button-role=\"goprev\"]";
+=======
+>>>>>>> 1fb214779473d822406ab203d5eeae116ddc7f4a
 
 function setDetails(imageUrl, titleText) {
-  'use strict';
+  "use strict";
   var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
-  detailImage.setAttribute('src', imageUrl);
+  detailImage.setAttribute("src", imageUrl);
 
   var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
   detailTitle.textContent = titleText;
 }
 
 function imageFromThumb(thumbnail) {
-  'use strict';
-  return thumbnail.getAttribute('data-image-url');
+  "use strict";
+  return thumbnail.getAttribute("data-image-url");
 }
 
 function titleFromThumb(thumbnail) {
-  'use strict';
-  return thumbnail.getAttribute('data-image-title');
+  "use strict";
+  return thumbnail.getAttribute("data-image-title");
 }
 
 function setDetailsFromThumb(thumbnail) {
-  'use strict';
+  "use strict";
   setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
 
 function addThumbClickHandler(thumb) {
-  'use strict';
-  thumb.addEventListener('click', function(event) {
+  "use strict";
+  thumb.addEventListener("click", function(event) {
     event.preventDefault();
     setDetailsFromThumb(thumb);
-  })
+  });
 }
 
 function getThumbnailsArray() {
-  'use strict';
+  "use strict";
   var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
   var thumbnailArray = [].slice.call(thumbnails);
   return thumbnailArray;
 }
 
+function nextImage() {
+  "use strict";
+  var thumbnails = getThumbnailsArray();
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  var detImgSrc = detailImage.getAttribute("src");
+  var index = 0;
+
+  for (var i = 0; i < thumbnails.length; i++){
+    if (detImgSrc == thumbnails[i].getAttribute("href")){
+      index = i + 1;
+      if (index == thumbnails.length){
+        index = 0;
+        break;
+      }
+      break;
+    }
+  }
+
+  setDetailsFromThumb(thumbnails[index]);
+}
+
+function prevImage() {
+  "use strict";
+  var thumbnails = getThumbnailsArray();
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  var detImgSrc = detailImage.getAttribute("src");
+  var index = 0;
+
+  for (var i = 0; i < thumbnails.length; i++){
+    if (detImgSrc == thumbnails[i].getAttribute("href")){
+      index = i - 1;
+      if (index < 0){
+        index = thumbnails.length - 1;
+        break;
+      }
+      break;
+    }
+  }
+
+  setDetailsFromThumb(thumbnails[index]);
+}
+
+function addNextPrevHandler() {
+  "use strict";
+  var prevButton = document.querySelector(PREV_BUTTON_SELECTOR);
+  var nextButton = document.querySelector(NEXT_BUTTON_SELECTOR);
+  prevButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    prevImage();
+  });
+
+  nextButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    nextImage();
+  });
+}
+
 function initializeEvents() {
-  'use strict';
+  "use strict";
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
+  addNextPrevHandler();
 }
 
 initializeEvents();
